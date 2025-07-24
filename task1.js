e57adc60321d08b0c7e05590cca013a2c7161f0a
Sridevisivaraj <sivar2156@gmail.com>
2025-07-24 19:30:20 +0530

    Task1:Conditional statement practice

---
+++ task1.js
@@ -0,0 +1,41 @@
+// Simple if statement
+let number = 5;
+if (number > 0) {
+    console.log("The number is positive.");
+}/
+// if else block
+let age = 17;
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}/
+// Mutiple condition with else if
+let score = 85;
+if (score >= 90) {
+    console.log("Grade A");
+} else if (score >= 80) {
+    console.log("Grade B");
+} else if (score >= 70) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
+// switch statement
+let day = 3;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+
+
